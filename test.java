c82ffaa27593bbd524f3a729fce28927f2223a30
shadyAray <shady159866@gmail.com>
2023-04-12 18:03:13 +0800

    私人练习

---
+++ test.java
@@ -0,0 +1,14 @@
+public class test
+{
+    public static void main(String[]args)
+    {
+        char x='魏',y='心',z='睿';
+        int a='\u4e2d',b='\u56fd';
+        System.out.println((int)x);
+        System.out.println((int)y);
+        System.out.println((int)z);
+        System.out.println((char)a);
+        System.out.println((char)b);
+    }
+        
+}
---
+++ 计算器.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class 计算器 
+{
+    public static void main(String[]args)
+    {
+        System.out.println("欢迎使用计算功能！");
+        System.out.println("请输入第一个数");
+        int a=();
+        System.out.println("请输入第二个数");
+        int b=();
+        System.out.println("合计="a+b);
+
+    }
+}
