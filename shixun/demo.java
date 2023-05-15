6ab5f37f4cfad4257b3b647a8a6d850c7c4aeb0c
shadyAray <shady159866@gmail.com>
2023-05-15 11:45:13 +0800

    1

---
+++ shixun/demo.java
@@ -0,0 +1,19 @@
+package shixun;
+import java.util.Scanner;
+public class demo {
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("        悦茶点餐系统\n");
+        System.out.print("********************************\n");
+        System.out.print("        1.登陆系统\n");
+        System.out.print("        2.退出");
+        System.out.print(">>>>>>>>\n");
+        System.out.print("请选择，请输入数字：");
+        scanner.nextInt();
+        System.out.print("请输入账号：");
+        scanner.nextInt();
+        System.out.print("请输入密码：");
+        scanner.nextInt();
+
+    }
+}
