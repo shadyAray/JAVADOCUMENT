a9da31f77dc3c8cd285aac1588de1ddd3a2ad379
shadyAray <shady159866@gmail.com>
2023-05-16 11:15:10 +0800

    上传实训作业2，删除一堆垃圾

--- shixun/day1/demo.java
+++ shixun/day1/demo.java
@@ -1,4 +1,4 @@
-package shixun;
+package shixun.day1;
 import java.util.Scanner;
 public class demo {
     public static void main(String []args){
---
+++ shixun/day2/day2.java
@@ -0,0 +1,33 @@
+package shixun.day2;
+import java.util.Scanner;
+public class day2 {
+    public static void main(String []args){
+        //主菜单功能
+        System.out.print("*********************************\n");
+        System.out.print("悦茶点餐系统->主菜单\n");
+        System.out.print("*********************************\n");
+        System.out.print("         1.会员管理\n         2.菜单管理\n         3.结算\n         4.积分消费\n         5.注销\n");
+        System.out.print(">>>>>>>>>>\n");
+        System.out.print("请选择，请输入数字:");
+        int a = new Scanner(System.in).nextInt();
+        //主菜单功能
+        //选项1功能
+        if (a==1){
+            System.out.print("悦茶点餐系统->主菜单->会员管理\n*********************************\n");
+            System.out.print("         1.显示所有会员信息\n         2.添加会员\n         3.修改会员\n         4.返回主菜单\n>>>>>>>>>>\n");
+            System.out.print("请选择，请输入数字：");
+            int a1 = new Scanner(System.in).nextInt();
+        //选项1功能
+        //选项1-1功能
+            if (a1==1){
+                System.out.print("*********************************\n");
+                System.out.print("悦茶点餐系统->主菜单->会员管理->显示所有会员信息\n>>>>>>>\n");
+                System.out.print("会员名  储值余额  会员积分  是否是会员\n");
+                System.out.print(" wxr      500       300         是\n");
+                System.out.print("魏心睿    200       -1         否\n");
+            }
+        //选项1-1功能
+            System.out.print("回车返回上一级>>>");
+        }
+    }
+}
