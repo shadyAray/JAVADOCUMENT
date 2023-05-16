1d6e21bc55d4646a9b88ec68a37754e1b003e024
shadyAray <shady159866@gmail.com>
2023-05-16 11:38:55 +0800

    加入功能二菜单管理的荤菜

--- shixun/day2/day2.java
+++ shixun/day2/day2.java
@@ -24,10 +24,27 @@ public class day2 {
                 System.out.print("悦茶点餐系统->主菜单->会员管理->显示所有会员信息\n>>>>>>>\n");
                 System.out.print("会员名  储值余额  会员积分  是否是会员\n");
                 System.out.print(" wxr      500       300         是\n");
-                System.out.print("魏心睿    200       -1         否\n");
+                System.out.print("魏心睿    200       -1          否\n");
             }
         //选项1-1功能
             System.out.print("回车返回上一级>>>");
         }
+        //选项2功能
+        if (a==2){
+            System.out.print("悦茶点餐系统->主菜单->菜单管理\n*********************************\n");
+            System.out.print("         1.荤菜\n         2.素菜\n         3.饮品\n>>>>>>>>>>\n");
+            System.out.print("请选择，请输入数字：");
+            int a2 = new Scanner(System.in).nextInt();
+        //选项2功能
+        //选项2-1功能
+        if (a2==1){
+            System.out.print("*********************************\n");
+            System.out.print("悦茶点餐系统->主菜单->会员管理->荤菜\n>>>>>>>\n");
+            System.out.print("            1.烤肉\n");
+            System.out.print("            2.烧鸡\n");
+            System.out.print("           3.啤酒鸭\n");
+        }
+        }
+        }
     }
-}
+
